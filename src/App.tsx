4f63ba36ab45aee11957e4ad8bab37d228f9edc4
Carl Smith <carlsmith101@live.co.uk>
2025-01-20 20:02:43 +0000

    -> Handler Functions in JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,65 @@
-const welcome = {
-  greeting: 'Hey',
-  title: 'React'
-};
+import { BaseSyntheticEvent, SyntheticEvent } from "react";
 
-function App() {
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1
+  }
+];
+
+const App = () => (
+  <div>
+    <h1>Development Journal</h1>
+
+    <Search />
+
+    <hr />
+
+    <List />
+  </div>
+);
+
+const Search = () => {
+  const handleChange = (event: BaseSyntheticEvent) => {
+
+    console.log(event);
+
+    console.log(event.target.value);
+  }
+
   return (
-    <>
-      <div>
-          <h1>{welcome.greeting} {welcome.title}</h1>
-
-          <label htmlFor="search">Search: </label>
-          <input id="search" type="text" />
-      </div>
-    </>
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange}/>
+    </div>
   );
 }
+
+const List = () => (
+  <ul>
+    {list.map(item => (
+      <li key={item.objectId}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default App;
