a1e439c385e58662d448c063762366f45f301afe
Carl Smith <carlsmith101@live.co.uk>
2025-02-16 20:22:49 +0000

    -> React Asynchronous Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, Fragment, useEffect, useState } from 'react';
+import React, { ChangeEvent, Fragment, HTMLInputTypeAttribute, MouseEventHandler, ReactNode, useEffect, useState } from 'react';
 
 type Story = {
   objectId: number
@@ -9,8 +9,34 @@ type Story = {
   points: number
 }
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1
+  }
+];
+
+const getAsyncStories = () => new Promise((resolve) => {
+  setTimeout(
+    () => resolve({ data: { stories: initialStories } }),
+    2000
+  )
+});
+
 const useStorageState = (key: string, initialState: string) => {
-  const [value, setValue] = useState (
+  const [value, setValue] = useState(
     localStorage.getItem(key) || initialState
   );
 
@@ -22,26 +48,26 @@ const useStorageState = (key: string, initialState: string) => {
 };
 
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectId: 0
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectId: 1
-    }
-  ];
+  const [searchTerm, setSearchTerm] = useStorageState(
+    'search',
+    'React'
+  );
 
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  const [stories, setStories] = useState<Story[]>([]);
+
+  useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories)
+    });
+  }, [])
+
+  const handleRemoveStory = (item: Story) => {
+    const newStories = stories.filter(
+      (story) => item.objectId !== story.objectId
+    );
+
+    setStories(newStories);
+  }
 
   const handleSearch = (
     event: ChangeEvent<HTMLInputElement>
@@ -49,57 +75,82 @@ const App = () => {
     setSearchTerm(event.target.value)
   };
 
-  const searchedStories = stories.filter((story) => 
+  const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
-);
+  );
 
   return (
     <div>
       <h1>Development Journal</h1>
-  
-      <Search search={searchTerm} onSearch={handleSearch}/>
-  
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        Search:
+      </InputWithLabel>
       <hr />
-  
-      <List list={searchedStories} />
+
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 }
 
-type SearchProps = {
-  search: string
-  onSearch: (event: ChangeEvent<HTMLInputElement>) => void;
+type InputWithLabelProps = {
+  id: string
+  type?: HTMLInputTypeAttribute
+  value: string
+  isFocused: boolean
+  onInputChange: (event: ChangeEvent<HTMLInputElement>) => void
+  children: ReactNode
 }
 
-const Search = ({ search, onSearch }: SearchProps) => (
+const InputWithLabel = ({
+  id,
+  type = 'text',
+  value,
+  isFocused,
+  onInputChange,
+  children
+}: InputWithLabelProps) => (
   <>
-    <label htmlFor="search">Search: </label>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
     <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
+      id={id}
+      type={type}
+      value={value}
+      autoFocus={isFocused}
+      onChange={onInputChange}
     />
   </>
 );
 
 type ListProps = {
-  list: Story[];
+  list: Story[]
+  onRemoveItem: (item: Story) => void
 };
 
-const List = ({ list }: ListProps) => (
+const List = ({ list, onRemoveItem }: ListProps) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectId} item={item} />
+      <Item
+        key={item.objectId}
+        item={item}
+        onRemoveItem={onRemoveItem}
+      />
     ))}
   </ul>
 );
 
 type ItemProps = {
-  item: Story;
+  item: Story
+  onRemoveItem: (item: Story) => void
 };
 
-const Item = ({ item }: ItemProps) => (
+const Item = ({ item, onRemoveItem }: ItemProps) => (
   <li>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -107,6 +158,11 @@ const Item = ({ item }: ItemProps) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type='button' onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button> 
+    </span>
   </li>
 );
 
